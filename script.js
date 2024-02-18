b618a857dd91c278e32a25b568a22c5c0516dc19
Stephen <stephensouness@outlook.com>
2024-02-18 15:34:07 +0000

    hash table tutorial followed

---
+++ script.js
@@ -0,0 +1,85 @@
+// function hash(key) {
+//   let hashCode = 0;
+
+//   const primeNumber = 31;
+//   for (let i = 0; i < key.length; i++) {
+//     hashCode = primeNumber * hashCode + key.charCodeAt(i);
+//   }
+//   return hashCode;
+// }
+
+// console.log(hash("Stephen"));
+
+// const person = {};
+// person["firstName"] = "jeev";
+// person["lastName"] = "jeevy";
+
+function hashStringToInt(string, tableSize) {
+  let hash = 17;
+  for (let i = 0; i < string.length; i++) {
+    hash = (13 * hash * string.charCodeAt(i)) % tableSize;
+  }
+
+  return hash;
+}
+
+class HashTable {
+  table = new Array(3);
+  numItems = 0;
+
+  resize = () => {
+    const newTable = new Array(this.table.length * 2);
+    this.table.forEach((item) => {
+      if (item) {
+        item.forEach(([key, value]) => {
+          const idx = hashStringToInt(key, newTable.length);
+          if (newTable[idx]) {
+            newTable[idx].push([key, value]);
+          } else {
+            newTable[idx] = [[key, value]];
+          }
+        });
+      }
+    });
+    this.table = newTable;
+  };
+
+  setItem = (key, value) => {
+    this.numItems++;
+    const loadFactor = this.numItems / this.table.length;
+    if (loadFactor > 0.8) {
+      //resize
+      this.resize();
+    }
+
+    const idx = hashStringToInt(key, this.table.length);
+    if (this.table[idx]) {
+      this.table[idx].push([key, value]);
+    } else {
+      this.table[idx] = [[key, value]];
+    }
+  };
+
+  getItem = (key) => {
+    const idx = hashStringToInt(key, this.table.length);
+    if (!this.table[idx]) {
+      return null;
+    }
+
+    //0(n)
+    return this.table[idx].find((x) => x[0] === key)[1];
+  };
+}
+
+const myTable = new HashTable();
+myTable.setItem("firstName", "jeevy");
+console.log(myTable.table.length);
+myTable.setItem("lastName", "ron");
+myTable.setItem("age", "5");
+myTable.setItem("dob", "1/2/79");
+
+console.log(myTable.getItem("firstName"));
+console.log(myTable.getItem("lastName"));
+console.log(myTable.getItem("age"));
+console.log(myTable.getItem("dob"));
+console.log(myTable.table.length);
